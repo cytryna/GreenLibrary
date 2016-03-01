b1a7adaf3df1694bbdeb9477a3b2cc08767e93dd
rwichrowski <radoslaw.wichrowski@efigence.com>
2016-03-01 07:49:57 +0100

    Zmiany zmiany zmiany

---
+++ src/main/java/s10338/main/HomeController.java
@@ -0,0 +1,17 @@
+package s10338.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Witaj w sklepie internetowym!");
+        model.addAttribute("tagline", "Wyjątkowym i jedynym sklepie internetowym");
+
+        return "welcome";
+    }
+}
